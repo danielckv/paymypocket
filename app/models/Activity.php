0a120be65e622088ff5e121a7eda9816c5fb249a
yoni <killer.in.red@gmail.com>
2014-02-07 02:36:50 +0200

    fixed models

--- app/models/Activity.php
+++ app/models/Activity.php
@@ -1,13 +1,6 @@
 <?php
 
-<<<<<<< HEAD
-use Illuminate\Auth\UserInterface;
-use Illuminate\Auth\Reminders\RemindableInterface;
-
-class Activity extends Eloquent implements UserInterface, RemindableInterface {
-=======
 class Activity extends Eloquent {
->>>>>>> yoni
 
 	/**
 	 * The database table used by the model.
